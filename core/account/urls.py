9f7d6a1009d7a89b90e54f494e71d2322e0314c9
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2023-01-02 03:20:56 +0330

    verifications
    Authentications and reset password

--- core/account/urls.py
+++ core/account/urls.py
@@ -6,12 +6,17 @@ app_name='account'
 
 urlpatterns = [
     path("register/",views.registerView,name='registerView'),
+    path('login/',views.Login,name='login'),
     path('sendsms1/',views.send_sms_test,name='send'),
     path('VerifyChecked/',views.VerifyChecked,name='verify'),
     path('complateprofile/',views.ComplateProfileView,name='comp'),
     path('complateform/',views.ComplateProfile,name='complate'),
     path('logout/',views.LogOut,name='logout'),
     path('resetpassword/',views.respass,name='resetpasswordsms'),
+    path('sendsms2/',views.SendSmsReset,name='send2'),
+    path('res/',views.ResetProfileView,name='ResetProfileView'),
+    path('Changepassword2/',views.ResetProfile,name='resetprofile'),
+    path('Change/',views.VerifyChecked2,name='changepass'),
 ]
 
 
--- core/account/views.py
+++ core/account/views.py
@@ -4,6 +4,7 @@ from kavenegar import *
 from django.contrib.auth import get_user_model
 import random
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import login,logout
 # Create your views here.
@@ -88,8 +89,74 @@ def respass(request):
     return render(request,'account/eghdam.html')
 
 
+def SendSmsReset(request):
+    number = random.randint(1000, 9999)
+    print(number)
+    if request.method == "POST":
+        phone_number = request.POST.get('phone_number')
+        if MyUser.objects.filter(phone_number=phone_number):
+            MyUser.objects.filter(phone_number=phone_number).update(token=number)
+        else:
+            messages.success(request,'شما هیج اکانتی ندارید')
+    else:
+        return render(request,'account/verify2.html')
+
+    # api = KavenegarAPI('4D526E3432522F42744D47414B3845436D59734377572B71645A455565644575')
+    # params = {'sender' : '10000080808880', 'receptor': f'{phone}', 'message' :f'{number}' }
+    # api.sms_send( params)
+    response = render(request,'account/verify2.html')
+    x =phone_number
+    response.set_cookie('phone_number_cookie',x,1000)
+    return response
 
 
+def ResetProfileView(request):
+    return render(request,'account/ResetPasswordView.html',{})
+
+
+def ResetProfile(request):
+    if request.method == "POST":
+        # username=request.POST.get('username')
+        password = request.POST.get('password')
+        phone_c = request.COOKIES['phone_number_cookie']
+        MyUser.objects.filter(phone_number=phone_c).update(
+            password=make_password(password)
+        )
+        messages.success(request,'عملیات با موفقیت انجام شد')
+    return redirect('/')
+
+
+def VerifyChecked2(request):
+    if request.method == "POST":
+        token = request.POST.get('token')
+        phone_c = request.COOKIES['phone_number_cookie']
+        try :
+            user = MyUser.objects.get(phone_number= phone_c)
+        except:
+            messages.success(request,'شما اکانتی با این شماره ندارید')
+
+        if user.token == token :
+            return render(request,'account/ResetPasswordView.html')
+        else:
+            messages.success(request,'!!! کدارسالی را درست وارد کنید')
+        return redirect('account/ResetPasswordView.html')
+    return render(request,'account/ResetPasswordView.html')
+
+
+def Login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user.is_verified:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.success(request,"شما احراز هویت نشده ایید")
+    form = AuthenticationForm()
+    return render(request,'account/login.html',{'form':form})
 
 def LogOut(request):
     logout(request)
