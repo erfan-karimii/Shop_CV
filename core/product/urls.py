73ca5b5646a6911bf0d137486bd7e9939d97430c
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2023-01-03 15:46:26 +0330

    15:46
    fix Bug

--- core/product/urls.py
+++ core/product/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
-from .views import index_view,listview
+from .views import index_view,listview,detailview
 
+app_name='product'
 
 urlpatterns = [
     path('product/',index_view,name='home'),
     path('listview/',listview,name='listview'),
+    path('det/<id>/',detailview,name='detailview'),
 ]
--- core/product/views.py
+++ core/product/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Product
 # Create your views here.
@@ -11,12 +11,21 @@ def index_view(request):
 
 def listview(request):
     posts = Product.objects.filter(is_active=True).order_by('-created')
-    paginator = Paginator(posts,20)
+    paginator = Paginator(posts,2)
     page_number = request.GET.get('page')
     posts = paginator.get_page(page_number)
     context = {
         'posts':posts,
         'count' : Product.objects.filter(is_active=True).count(),
-        # 'paginator':Paginator.object_list
+        # 'listobj' : posts.object_list.count()
         }
     return render(request,'listview.html',context)
+
+
+def detailview(request,id):
+    post = get_object_or_404(Product,id=id)
+
+    context = {
+        'post':post
+    }
+    return render(request,'detailview.html',context)
