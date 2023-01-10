f5829bd7ff63f07cc72e9ce8e8914b1f1e526618
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2023-01-10 19:42:38 +0330

    cms

--- core/cms/urls.py
+++ core/cms/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'cms'
 
 urlpatterns = [
-    path("",views.test,name='test')
+    path('navone/',views.NavOneCreateView.as_view(),name='navone'),
+    path('navonelist/',views.NavOneListView.as_view(),name='nav_list'),
+    path('detail/navone/<pk>/',views.NavOneDetailView.as_view(),name='detail_navone'),
+    path('delete/navone/<pk>/',views.NavOneDeleteView.as_view(),name='nav_delete'),
 ]
--- core/cms/views.py
+++ core/cms/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import (
+    CreateView, ListView  , UpdateView , View)
 # Create your views here.
+from home.models import NavOne
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
+#-----------Start---------NavOne-----------------
 
+class NavOneCreateView(CreateView):
+    model = NavOne
+    fields = "__all__"
+    template_name = 'cms/nav/nav_one.html'
+    success_url = reverse_lazy("cms:nav_list")
 
-def test(request):
-    pass
+    def form_valid(self,form):
+        messages.success(self.request,'با موفقیت ثبت شد')
+        return super().form_valid(form)
+
+class NavOneListView(ListView):
+    model = NavOne
+    template_name = 'cms/nav/nav_list.html'
+    context_object_name = 'navsone'
+
+class NavOneDetailView(UpdateView):
+    model = NavOne
+    template_name = 'cms/nav/nav_one_detail.html'
+    context_object_name = 'navone'
+    fields="__all__"
+    success_url = reverse_lazy("cms:nav_list")
+    def form_valid(self,form):
+        messages.success(self.request,'با موفقیت ویرایش شد')
+        return super().form_valid(form)
+
+class NavOneDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        NavOne.objects.get(id=kwargs['pk']).delete()
+        messages.success(self.request,'با موفقیت حذف شد')
+        return redirect("cms:nav_list")
+
+#-------------End-------NavOne-----------------
