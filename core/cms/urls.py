4b0ae904e04f8fa8a2809632fa56fb783353b710
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2023-01-11 01:21:11 +0330

    Cms

--- core/cms/urls.py
+++ core/cms/urls.py
@@ -5,8 +5,23 @@ from . import views
 app_name = 'cms'
 
 urlpatterns = [
+    #-----------Start---------NavOne-----------------
     path('navone/',views.NavOneCreateView.as_view(),name='navone'),
     path('navonelist/',views.NavOneListView.as_view(),name='nav_list'),
     path('detail/navone/<pk>/',views.NavOneDetailView.as_view(),name='detail_navone'),
     path('delete/navone/<pk>/',views.NavOneDeleteView.as_view(),name='nav_delete'),
+
+    #-----------Start---------NavTwo-----------------
+    path('navtwo/',views.NavTwoCreateView.as_view(),name='navtwo'),
+    path('detail/navtwo/<pk>/',views.NavTwoDetailView.as_view(),name='detail_navtwo'),
+    path('delete/navtwo/<pk>/',views.NavTwoDeleteView.as_view(),name='navtwo_delete'),
+
+    #---------Start-----------FooterOne------------------------
+    path('footer_one/',views.FooterOneCreateView.as_view(),name='footer_asli'),
+    path('footer_list/',views.FooterListView.as_view(),name='footer_list'),
+    path('detail/footer_one/<pk>/',views.FooterDetailView.as_view(),name='footer_asli_detail'),
+    path('delete/footer_one/<pk>/',views.FooterOneDeleteView.as_view(),name='footer_one_delete'),
+
+    #----------Start-----------FooterTwo---------------------
+    path('detail/footer_two/<pk>/',views.FooterZirDetailView.as_view(),name='footer_zir_detail'),
 ]
--- core/cms/views.py
+++ core/cms/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.views.generic import (
     CreateView, ListView  , UpdateView , View)
 # Create your views here.
-from home.models import NavOne
+from home.models import NavOne,NavTwo,FooterOne,FooterTwo
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
@@ -18,11 +18,20 @@ class NavOneCreateView(CreateView):
     def form_valid(self,form):
         messages.success(self.request,'با موفقیت ثبت شد')
         return super().form_valid(form)
+    
+    
 
 class NavOneListView(ListView):
     model = NavOne
     template_name = 'cms/nav/nav_list.html'
     context_object_name = 'navsone'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['navstwo'] =NavTwo.objects.all()
+        return context
 
 class NavOneDetailView(UpdateView):
     model = NavOne
@@ -41,3 +50,90 @@ class NavOneDeleteView(View):
         return redirect("cms:nav_list")
 
 #-------------End-------NavOne-----------------
+
+
+
+#-----------Start---------NavTwo-----------------
+class NavTwoCreateView(CreateView):
+    model = NavTwo
+    fields = "__all__"
+    template_name = 'cms/nav/nav_Two.html'
+    success_url = reverse_lazy("cms:nav_list")
+
+    def form_valid(self,form):
+        messages.success(self.request,'با موفقیت ثبت شد')
+        return super().form_valid(form)
+
+class NavTwoDetailView(UpdateView):
+    model = NavTwo
+    template_name = 'cms/nav/nav_two_detail.html'
+    context_object_name = 'navtwo'
+    fields="__all__"
+    success_url = reverse_lazy("cms:nav_list")
+    def form_valid(self,form):
+        messages.success(self.request,'با موفقیت ویرایش شد')
+        return super().form_valid(form)
+
+
+class NavTwoDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        NavTwo.objects.get(id=kwargs['pk']).delete()
+        messages.success(self.request,'با موفقیت حذف شد')
+        return redirect("cms:nav_list")
+#-------------End-------NavTwo-----------------
+
+
+#--------------Start----------Footer--------------
+class FooterOneCreateView(CreateView):
+    model = FooterOne
+    fields = "__all__"
+    template_name = 'cms/footer/footer_asli.html'
+    success_url = reverse_lazy("cms:footer_list")
+
+    def form_valid(self,form):
+        messages.success(self.request,'با موفقیت ثبت شد')
+        return super().form_valid(form)
+
+
+class FooterListView(ListView):
+    model = FooterOne
+    template_name = 'cms/footer/footer_list.html'
+    context_object_name = 'footers'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['footer_zir'] =FooterTwo.objects.all()
+        return context
+
+class FooterDetailView(UpdateView):
+    model = FooterOne
+    template_name = 'cms/footer/footer_asli_detail.html'
+    context_object_name = 'footerasil'
+    fields="__all__"
+    success_url = reverse_lazy("cms:footer_list")
+    def form_valid(self,form):
+        messages.success(self.request,'با موفقیت ویرایش شد')
+        return super().form_valid(form)
+
+
+class FooterOneDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        FooterOne.objects.get(id=kwargs['pk']).delete()
+        messages.success(self.request,'با موفقیت حذف شد')
+        return redirect("cms:nav_list")
+
+#--------------End----------Footer--------------
+
+
+#--------------Start----------Footer_zir--------------
+class FooterZirDetailView(UpdateView):
+    model = FooterTwo
+    template_name = 'cms/footer/footer_zir_detail.html'
+    context_object_name = 'footer_zir'
+    fields="__all__"
+    success_url = reverse_lazy("cms:footer_list")
+    def form_valid(self,form):
+        messages.success(self.request,'با موفقیت ویرایش شد')
+        return super().form_valid(form)
